46748f5ceb05c20558a965fe12cad4b8aa94312a
baykonduberke <baykonduberke2@gmail.com>
2026-02-04 04:54:04 +0300

    geçici test apileri eklendi

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 
+import requests
 from fastapi import FastAPI
 
 from app.core.config import get_settings
@@ -43,6 +44,49 @@ async def root():
 @app.get("/health")
 async def health():
     return {"status": "healthy"}
+
+
+@app.get("/apitest")
+async def apitest():
+    url = "https://v3.football.api-sports.io/players/league=203"
+
+    payload = {}
+    headers = {
+        "x-apisports-key": "c8c106cd827858adfbd000e6d049b3e6",
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.json()
+
+
+@app.get("/besiktas-fikstur")
+async def get_besiktas_fixtures():
+    url = "https://v3.football.api-sports.io/fixtures"
+
+    params = {"team": "549", "season": "2024"}
+
+    headers = {
+        "x-apisports-key": "c8c106cd827858adfbd000e6d049b3e6",
+        "x-rapidapi-host": "v3.football.api-sports.io",
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+
+    return data
+
+
+@app.get("/mac-istatistik/{fixture_id}")
+async def get_match_statistics(fixture_id: int):
+    url = "https://v3.football.api-sports.io/fixtures/statistics"
+    params = {"fixture": fixture_id}
+    headers = {
+        "x-apisports-key": "c8c106cd827858adfbd000e6d049b3e6",
+        "x-rapidapi-host": "v3.football.api-sports.io",
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
 
 
 app.include_router(auth.router)
